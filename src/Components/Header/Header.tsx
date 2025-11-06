b73e451157305ef1d303b6f2e9bf1a64326ec166
ahmedsami019 <easami019192@gmail.com>
2025-11-06 21:27:59 +0600

    do some functional work to show more and less product in products section

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -4,7 +4,7 @@ import banner from "../../assets/Perfect-Ecommerce-Sales-Banner.webp"
 
 const Header = () => {
     return (
-        <div className='flex flex-col lg:flex-row justify-center gap-10 py-10 w-11/12 mx-auto'>
+        <div className='flex flex-col lg:flex-row justify-center gap-10 py-10 w-11/12 mx-auto mt-20'>
             <div className='lg:w-6/12 mx-auto space-y-5 text-center lg:text-start'>
                 <h2 className='text-5xl lg:text-7xl font-bold'>It's time to Explore something new </h2>
                 <p className='text-2xl text-gray-700'>we have too much collection of every type of products. From our website you get all original products. </p>
--- src/Components/Products/Products.tsx
+++ src/Components/Products/Products.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
+import Product from '../Product/Product';
+import { ArrowDownToLine, ArrowUpToLine } from 'lucide-react';
 
 const Products = () => {
 
@@ -15,12 +17,23 @@ const Products = () => {
             console.log(error);
         }
     }, [])
-    console.log(products);
+
+    const [showAll, setShowAll] = useState(false)
+    const updatedProducts = showAll? products: products.slice(0, 8)
+    // console.log(products);
 
 
     return (
         <div>
-            <h2>this is products section</h2>
+            <h2 className='text-2xl'>this is products section</h2>
+            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
+                {
+                    updatedProducts.map((product, index) => <Product key={index} product={product}></Product>)
+                }
+            </div>
+            <div className='flex justify-center'>
+                <button onClick={()=> setShowAll(!showAll)} className='btn btn-main my-10'>{!showAll? "Show all": "Show less"} {!showAll?<ArrowDownToLine size={22}></ArrowDownToLine>: <ArrowUpToLine size={22}></ArrowUpToLine>}</button>
+            </div>
         </div>
     );
 };
