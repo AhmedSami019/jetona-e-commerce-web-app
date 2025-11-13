7bfb7055e9fb2b1247641d57bdbaa2924c7aa31f
ahmedsami019 <easami019192@gmail.com>
2025-11-13 14:09:26 +0600

    added add to cart and buy now button and function in products details section

--- src/Pages/ProductsDetails/ProductsDetails.tsx
+++ src/Pages/ProductsDetails/ProductsDetails.tsx
@@ -1,12 +1,24 @@
 import { Star } from "lucide-react";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
-import ProductCounter from "../../Components/ProductCounter/ProductCounter";
 
 const ProductsDetails = () => {
   // this is id form loader of route
   const { productId } = useParams();
   const [product, setProduct] = useState({});
+
+  //   this is product counter section
+  const [count, setCount] = useState(1);
+
+  const handleIncrement = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  const handleDecrement = () => {
+    if (count > 1) {
+      setCount((prev) => prev - 1);
+    }
+  };
 
   useEffect(() => {
     fetch("/product.json")
@@ -19,15 +31,15 @@ const ProductsDetails = () => {
       });
   }, [productId]);
 
-  const { name, category, price, stoke, rating, description, image } = product;
+  const { name, category, currency, price, stoke, rating, description, image } = product;
 
   return (
     <div className="flex flex-col md:flex-row gap-10 md:gap-20 p-10 mt-20 w-11/12 mx-auto min-h-screen">
-      <div className="">
-        <img className="rounded-xl" src={image} alt="" />
+      <div className="md:w-6/12 ">
+        <img className="rounded-xl w-full" src={image} alt="" />
       </div>
       {/* details section of product */}
-      <div className="space-y-5">
+      <div className="md:w-6/12 space-y-5">
         <h2 className="text-4xl font-bold">{name}</h2>
         <div className="flex items-center text-lg font-medium gap-10">
           <p className="">Ratting:</p>
@@ -35,12 +47,34 @@ const ProductsDetails = () => {
             {rating} <Star stroke="0" fill="gold"></Star>
           </p>
         </div>
-          <ProductCounter></ProductCounter>
+        {/* price */}
+        <h2 className="text-2xl font-medium">${price}  {currency}</h2>
+        <p className="text-xl">{description}</p>
+        {/* this is counter section */}
+        <div className="flex gap-5 items-center">
+          <div className="flex gap-20 h-8">
+            <div className="bg-gray-200 flex justify-around items-center rounded-full">
+              <button
+                className="bg-gray-300 px-4 py-1 rounded-l-2xl"
+                onClick={handleDecrement}
+              >
+                −
+              </button>
+              <span className="px-3">{count}</span>
+              <button
+                className="bg-gray-300 px-4 py-1 rounded-r-2xl"
+                onClick={handleIncrement}
+              >
+                +
+              </button>
+            </div>
+          </div>
+        <button className="btn btn-outline-main border-[">Add to cart</button>
+        <button className="btn btn-main">Buy now</button>
+        </div>
       </div>
       {/* similar product section */}
-      <div>
-
-      </div>
+      <div></div>
     </div>
   );
 };
