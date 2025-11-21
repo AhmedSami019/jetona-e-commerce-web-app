db41e65cb66f23e007e32341dfedbfea47c899c3
ahmedsami019 <easami019192@gmail.com>
2025-11-21 16:23:14 +0600

    added new content into about section

--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import service from "../../assets/service-24-7-concept-illustration_114360-7380.webp";
+import founderImg from "../../assets/464619541_865480199005121_9009118941012390343_n.jpg"
 
 const About = () => {
   const [about, setAbout] = useState({});
@@ -14,8 +15,15 @@ const About = () => {
       });
   }, []);
 
-  const { founded, founder, headquarters, mission, vision, history, awards }: object =
-    about;
+  const {
+    founded,
+    founder,
+    headquarters,
+    mission,
+    vision,
+    history,
+    awards,
+  }: object = about;
   console.log(awards);
 
   return isDataLoaded === false ? (
@@ -24,15 +32,23 @@ const About = () => {
     <>
       <div className="mt-20 w-11/12 mx-auto space-y-2.5">
         {/* about section */}
-        <div className="pt-10  space-y-3.5">
-          <h2 className="text-5xl font-semibold">
-            About <span className="text-[#ff4f00]">jeTona</span>
-          </h2>
-          <p className="lg:w-5/12 text-xl text-gray-700">{mission}</p>
-          <p className="text-3xl font-medium italic">Founded in {founded}</p>
-          <p className="text-lg lg:w-6/12">
-            <span className="font-bold">History:</span> {history}
-          </p>
+        <div className="pt-10 flex flex-col lg:flex-row gap-5 justify-between bg-gray-200 px-5 py-20 rounded-xl">
+          <div className="space-y-4 lg:w-6/12">
+            <h2 className="text-5xl font-semibold">
+              About <span className="text-[#ff4f00]">jeTona</span>
+            </h2>
+            <p className="lg:w-10/12 text-xl text-gray-700">{mission}</p>
+            <p className="text-3xl font-medium italic">Founded in {founded}</p>
+            <p className="text-lg lg:w-10/12">
+              <span className="font-bold">History:</span> {history}
+            </p>
+          </div>
+
+          <div className="lg:w-6/12 text-center space-y-3 lg:text-start">
+            <h2 className="text-5xl font-semibold">Emtiaz Ahmed <span className="text-[#ff4f00]">Sami</span></h2>
+            <h2 className="text-2xl text-gray-500 italic">The Founder</h2>
+            <div className="h-99 flex justify-center"><img className="h-full rounded-xl" src={founderImg} alt="" /></div>
+          </div>
         </div>
         {/* mission and vision section */}
         <div className=" pt-10 flex flex-col lg:flex-row-reverse justify-between gap-5 mt-10">
@@ -47,18 +63,18 @@ const About = () => {
 
             <p></p>
           </div>
-          <div className="lg:w-6/12">
-            <img className="lg:w-8/12" src={service} alt="image is coming" />
+          <div className="lg:w-6/12  rounded-xl">
+            <img className="lg:w-8/12 bg-gray-200 p-5 rounded-xl" src={service} alt="image is coming" />
           </div>
         </div>
 
-{/* founder section */}
-<div>
-    <div>
-        <img src="" alt="" />
-    </div>
-    <div></div>
-</div>
+        {/* founder section */}
+        <div>
+          <div>
+            <img src="" alt="" />
+          </div>
+          <div></div>
+        </div>
 
         {/* award section */}
         <div className="pt-10 flex flex-col lg:flex-row justify-between gap-5 mt-10">
@@ -77,9 +93,7 @@ const About = () => {
           </div>
           <div className="text-start lg:w-6/12">
             <h2 className="text-5xl font-semibold">Contact</h2>
-            {
-
-            }
+            {}
           </div>
         </div>
       </div>
